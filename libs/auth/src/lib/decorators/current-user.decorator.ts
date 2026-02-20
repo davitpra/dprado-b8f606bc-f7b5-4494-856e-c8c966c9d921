e30279d655887cace7acd8d7ba0ef3a14d0cf2db
David Prado <j.david.prado1716@gmail.com>
2026-02-19 22:15:53 -0500

    setup Monorepo - added libs

---
+++ libs/auth/src/lib/decorators/current-user.decorator.ts
@@ -0,0 +1,2 @@
+// TODO: Implement CurrentUser decorator
+export {};
---
+++ libs/data/src/lib/enums/index.ts
@@ -0,0 +1,4 @@
+export * from './task-status.enum.js';
+export * from './task-category.enum.js';
+export * from './task-priority.enum.js';
+export * from './user-role.enum.js';
