b3d99aed6eaa4fc159ba70dd952f6595a6b40704
David Prado <j.david.prado1716@gmail.com>
2026-02-19 21:19:38 -0500

    configuration of carpets and install dependancies

---
+++ apps/dashboard/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    'apps/dashboard/src/**/*.{html,ts}',
+    'libs/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
